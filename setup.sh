8650b5143605329b218224a06ac5a00214dfc18f
Steve Clarke <doctorbh@ninjanizr.com>
2011-11-06 19:46:30 -0330

    added skeleton vim config

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 ln -s ~/dotfiles/bash/bash_profile.symlink ~/.bash_profile
 ln -s ~/dotfiles/bin ~/bin
+ln -s ~/dotfiles/vim/vimrc.symlink ~/.vimrc
+ln -s ~/dotfiles/vim/vim.symlink ~/.vim
