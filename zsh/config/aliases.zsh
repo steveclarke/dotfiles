7cb3af9d0daa79c4492fd8ea023071b17856e9cd
Steve Clarke <doctorbh@ninjanizr.com>
2011-11-17 19:09:37 -0330

    Core zsh config complete
    * mostly working as expected.
    * not yet stable as I don't yet fully understand everything in there

---
+++ zsh/config/aliases.zsh
@@ -0,0 +1,16 @@
+alias ff='clear'
+alias reload='. ~/.zshrc'
+
+alias ..='cd ..'
+
+# ls
+alias ls='ls -F'
+alias ll='ls -lF'
+alias la='ls -A'
+
+# vim
+alias m='mvim'
+alias v='vim'
+
+# git
+alias gs='git status'
