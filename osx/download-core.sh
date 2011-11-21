6b7a6b6b5a49f1c9ba6ab760691c1825b06d3f41
Steve Clarke <doctorbh@ninjanizr.com>
2011-11-21 14:11:53 -0330

    Start of a standard bootstrap downloader for OS X

---
+++ osx/download-core.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Download core programs that I want on every Mac
+
+cd ~/Downloads
+
+# Google Chrome
+echo "Downloading Google Chrome..."
+curl -O https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
+
+# f.lux
+echo "Downloading f.lux..."
+curl -O https://secure.herf.org/flux/Flux.zip
