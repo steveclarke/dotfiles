192b9269bda8a040d6dc23b2c178f245dca1b312
Steve Clarke <doctorbh@ninjanizr.com>
2011-11-23 10:39:23 -0330

    Add ruby-config to set up ruby (gems only atm)

---
+++ ruby/ruby-config.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+gem install rake
+gem install pg
+gem install bundler
+gem install capistrano
+gem install mysql
+gem install heroku
+gem install lunchy
+gem install rails
+gem install cheat
+gem install ffaker
+
