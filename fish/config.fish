1a4f803b18cb52fa30a3d052cb2d96e334b977ad
Steve Clarke <steve@sevenview.ca>
2023-04-04 19:21:52 -0230

    add Fish shell

---
+++ fish/config.fish
@@ -0,0 +1,25 @@
+source ~/.asdf/asdf.fish
+source ~/.asdf/completions/asdf.fish
+
+# FUNTOOO KEYCHAIN - frontend to ssh-agent
+# https://www.funtoo.org/Funtoo:Keychain
+# if status is-interactive
+#   /usr/bin/keychain --nogui $HOME/.ssh/sevenview2020 $HOME/.ssh/sevenview
+#   source $HOME/.keychain/$(hostname)-fish
+# end
+
+# DIRENV
+# Source this after asdf so it's path doesn't override any path set in .envrc
+# https://lifesaver.codes/answer/fish-direnv-is-no-longer-triggered-on-shell-creation-583
+# This solves a problem where the .envrc wasn't parsed immediately after
+# VSCode opens a new terminal instance.
+if status is-interactive
+  direnv hook fish | source
+  direnv export fish | source
+end
+
+if status is-interactive
+	abbr -a -- ff clear
+	abbr -a -- upgrade "sudo nala upgrade"
+	abbr -a kill-server-pid "kill -QUIT \$(cat tmp/pids/server.pid)"
+end
