ea20d3fdc70846acab29a820429450115de22ce3
Steve Clarke <steve@sevenview.ca>
2023-12-03 19:48:51 -0330

    nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,27 @@
--- TODO: Write me
-print "Hello Neovim!"
+require("core")
 
+local opt = vim.opt
+
+-- [[ Indentation ]]
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.expandtab = true
+opt.smartindent = true
+
+-- [[ Line numbers ]]
+opt.number = true
+opt.relativenumber = true
+opt.numberwidth = 2
+
+-- [[ Window splitting ]]
+opt.splitbelow = true
+opt.splitright = true
+
+-- highlight the current line
+opt.cursorline = true
+
+-- always show the sign column, otherwise it would shift the text each time
+opt.signcolumn = "yes"
+
+-- enable 24-bit color
+opt.termguicolors = true
