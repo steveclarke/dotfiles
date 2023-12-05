fdb5d29d61b843ef61fc7ca2885c62bc05eed5d0
Steve Clarke <steve@sevenview.ca>
2023-12-05 13:29:23 -0330

    set timeout

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,3 +1,4 @@
 return {
-  { "folke/neoconf.nvim", cmd = "Neoconf" },
+  "folke/neoconf.nvim",
+  cmd = "Neoconf",
 }
--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -2,4 +2,8 @@ return {
   "folke/which-key.nvim",
   lazy = false,
   config = {},
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+  end,
 }
