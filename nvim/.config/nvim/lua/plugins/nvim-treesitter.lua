11ced0c73acfeb78185e4557bb2988c1c9546974
Steve Clarke <steve@sevenview.ca>
2023-12-07 22:53:18 -0330

    configure treesitter incremental seleciton

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -33,6 +33,15 @@ local config = function()
       "yaml",
     },
     auto_install = true,
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      },
+    },
   })
 end
 
