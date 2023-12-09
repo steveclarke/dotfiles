40fd6aedf21eae7c23c1cfe47206e9ba41900e6c
Steve Clarke <steve@sevenview.ca>
2023-12-09 09:36:58 -0330

    keychain

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,7 +5,8 @@ source ~/.asdf/completions/asdf.fish
 # https://www.funtoo.org/Funtoo:Keychain
 # if status is-interactive
 #   /usr/bin/keychain --nogui $HOME/.ssh/sevenview2020 $HOME/.ssh/sevenview
-#   source $HOME/.keychain/$(hostname)-fish
+#   /usr/bin/keychain --nogui $HOME/.ssh/sevenview2020 $HOME/.ssh/sevenview2020
+#   source $HOME/.keychain/$hostname-fish
 # end
 
 # DIRENV
