d7b6f4d4ee8e8565d8674f4fd9126981545f6a84
Steve Clarke <steve@sevenview.ca>
2023-12-09 19:52:21 -0330

    ensure_installed

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,4 +1,4 @@
-local opts = {
+local mason_opts = {
   ui = {
     icons = {
       package_installed = "✓",
@@ -8,22 +8,30 @@ local opts = {
   },
 }
 
+local lsp_config_opts = {
+  ensure_installed = {
+    "cssls",
+    "docker_compose_language_service",
+    "dockerls",
+    "emmet_ls",
+    "html",
+    "lua_ls",
+    "standardrb",
+    "tailwindcss",
+    "tsserver",
+  },
+}
+
 return {
   "williamboman/mason.nvim",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
   },
   lazy = false,
-  opts = opts,
-  -- FIXME: ensure_installed complains about naming the ensure_installed correctly
-  -- config = function()
-  --   local mason = require("mason")
-  --   local mason_lspconfig = require("mason-lspconfig")
-  --
-  --   mason_lspconfig.setup({
-  --     ensure_installed = {
-  --       "standardrb"
-  --     }
-  --   })
-  -- end,
+  config = function()
+    local mason = require("mason")
+    local mason_lspconfig = require("mason-lspconfig")
+    mason.setup(mason_opts)
+    mason_lspconfig.setup(lsp_config_opts)
+  end,
 }
