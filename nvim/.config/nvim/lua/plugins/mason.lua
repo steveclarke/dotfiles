5852b236358f32ec10225bddf835f5e272cc5891
Steve Clarke <steve@sevenview.ca>
2023-12-11 17:04:40 -0330

    code completion with lsp

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -24,13 +24,38 @@ local ensure_installed = {
 }
 
 local config = function()
+  local lspconfig = require("lspconfig")
+  local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
   local mason = require("mason")
   local mason_lspconfig = require("mason-lspconfig")
-  local lspconfig = require("lspconfig")
 
   local default_setup = function(server)
-    lspconfig[server].setup({})
+    lspconfig[server].setup({
+      capabilities = lsp_capabilities,
+    })
   end
+
+  vim.api.nvim_create_autocmd("LspAttach", {
+    desc = "Lsp Actions",
+    callback = function(event)
+      local opts = { buffer = event.buf }
+
+      vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+      vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+      vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+      vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+      vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+      vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+      vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+      vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+      vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+      vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+
+      vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>", opts)
+      vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts)
+      vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts)
+    end,
+  })
 
   -- Setup Mason
   mason.setup(mason_opts)
@@ -42,12 +67,32 @@ local config = function()
       -- Lua
       lua_ls = function()
         lspconfig.lua_ls.setup({
-          Lua = {
-            runtime = {
-              version = "LuaJIT",
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { "vim" },
+              },
             },
           },
         })
+      end,
+    },
+  })
+
+  local cmp = require("cmp")
+  cmp.setup({
+    sources = {
+      { name = "nvim_lsp" },
+    },
+    mapping = cmp.mapping.preset.insert({
+      -- Enter key confirms completion item
+      ["<CR>"] = cmp.mapping.confirm({ select = false }),
+      -- Ctrl + space triggers completion menu
+      ["<C-Space>"] = cmp.mapping.complete(),
+    }),
+    snippet = {
+      expand = function(args)
+        require("luasnip").lsp_expand(args.body)
       end,
     },
   })
@@ -68,6 +113,9 @@ return {
     dependencies = {
       "williamboman/mason-lspconfig.nvim",
       "neovim/nvim-lspconfig",
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-nvim-lsp",
+      "L3MON4D3/LuaSnip",
     },
     lazy = false,
     config = config,
--- nvim/.config/nvim/lua/plugins/nvim-lint.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -1,6 +1,6 @@
 return {
   "mfussenegger/nvim-lint",
-  enabled = false,
+  enabled = true,
   event = {
     "BufReadPre",
     "BufNewFile",
