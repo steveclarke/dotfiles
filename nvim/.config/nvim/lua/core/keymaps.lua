478b2af9ab6867f561bb3c6caa88efe7b7373a0b
Steve Clarke <steve@sevenview.ca>
2023-12-13 13:12:43 -0330

    alt-u/i to move lines

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -18,13 +18,14 @@ keymap.set("n", "U", "<C-r>", opts)
 -- Clear search highlight
 keymap.set("n", "<leader>/", "<cmd>noh<CR>", { desc = "Clear search highlights" })
 
--- Move current line / block with Alt-j/k ala vscode.
-keymap.set("n", "<A-j>", ":m .+1<CR>==", opts)
-keymap.set("n", "<A-k>", ":m .-2<CR>==", opts)
-keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
-keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
-keymap.set("v", "<A-j>", ":m '>+1<CR>gv-gv", opts)
-keymap.set("v", "<A-k>", ":m '<-2<CR>gv-gv", opts)
+-- Move current line / block with Alt-u/i ala vscode.
+-- Note: not using Alt-j/k because they're used by Zellij
+keymap.set("n", "<A-u>", ":m .+1<CR>==", opts)
+keymap.set("n", "<A-i>", ":m .-2<CR>==", opts)
+keymap.set("i", "<A-u>", "<Esc>:m .+1<CR>==gi", opts)
+keymap.set("i", "<A-i>", "<Esc>:m .-2<CR>==gi", opts)
+keymap.set("v", "<A-u>", ":m '>+1<CR>gv-gv", opts)
+keymap.set("v", "<A-i>", ":m '<-2<CR>gv-gv", opts)
 
 -- Better indenting
 keymap.set("v", "<", "<gv", opts)
@@ -36,7 +37,7 @@ keymap.set("v", ">", ">gv", opts)
 -- open splits with new buffers
 keymap.set("n", "-ss", ":new<cr>", opts)
 keymap.set("n", "-vv", ":vnew<cr>", opts)
-keymap.set("n", "-tb", ":tabnew %<cr>", opts)
+keymap.set("n", "-tb", ":tabnew %<cr>", opts) -- open current buffer in new tab
 keymap.set("n", "-tt", ":tabnew<cr>", opts)
 
 -- Commenting - Ctrl-/ to toggle comments in normal/visual mode.
