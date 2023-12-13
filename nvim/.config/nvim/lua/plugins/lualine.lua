81ab042162fa32fb8aec86b87a7da5645d98c00e
Steve Clarke <steve@sevenview.ca>
2023-12-12 23:19:40 -0330

    nvim focus of the day (wip)

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,11 +1,50 @@
+-- local tip_file_path = os.getenv("HOME") .. "/dotfiles/nvim/.config/nvim/data/tips.txt"
+--
+-- local count_lines = function(filename)
+--   local count = 0
+--   for _ in io.lines(filename) do
+--     count = count + 1
+--   end
+--   return count
+-- end
+--
+-- local tip = function()
+--   local tip_file = io.open(tip_file_path, "r")
+--
+--   if not tip_file then
+--     print("Error opening tip file " .. tip_file_path)
+--     return nil
+--   end
+--
+--   if count_lines == 0 then
+--     print("Tip file is empty")
+--     tip_file:close()
+--     return nil
+--   end
+--
+--   local random_line_number = math.random(1, count_lines(tip_file_path))
+--   local current_line = 0
+--
+--   for line in tip_file:lines() do
+--     current_line = current_line + 1
+--     if current_line == random_line_number then
+--       tip_file:close()
+--       return " Today's focus:  " .. line
+--     end
+--   end
+-- end
+
 local config = function()
   require("lualine").setup({
     options = {
-      theme = theme,
+      theme = "catppuccin",
       globalstatus = true,
+      section_separators = { left = "", right = "" },
+      component_separators = { left = "", right = "" },
     },
     sections = {
       lualine_a = { "buffers" },
+      -- lualine_c = { tip },
     },
   })
 end
