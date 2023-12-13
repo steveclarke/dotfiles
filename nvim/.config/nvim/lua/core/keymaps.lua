c58905dbf717e7c4d891b1dc7b4400f79bf8d7a5
Steve Clarke <steve@sevenview.ca>
2023-12-13 16:55:28 -0330

    allow merging of default keymap options

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -1,60 +1,68 @@
 local keymap = vim.keymap
+local default_options = { noremap = true, silent = true }
 
-local opts = { noremap = true, silent = true }
+local options = function(options)
+  return vim.tbl_extend("force", default_options, options or {})
+end
 
 -- Nvim Tree Explorer
-keymap.set("n", "<leader>e", vim.cmd.NvimTreeFocus, { desc = "Focus file explorer" })
-keymap.set("n", "<leader>fe", vim.cmd.NvimTreeToggle, { desc = "Toggle file explorer" })
+-- keymap.set("n", "<leader>e", vim.cmd.NvimTreeFocus, { desc = "Focus file explorer" })
+keymap.set("n", "<leader>e", vim.cmd.NvimTreeFocus, options({ desc = "Focus file explorer" }))
+keymap.set("n", "<leader>fe", vim.cmd.NvimTreeToggle, options({ desc = "Toggle file explorer" }))
 
 -- Map Ctrl-l to move to the window on the right
-keymap.set("n", "<C-l>", "<C-w>l", opts)
-keymap.set("n", "<C-h>", "<C-w>h", opts)
-keymap.set("n", "<C-j>", "<C-w>j", opts)
-keymap.set("n", "<C-k>", "<C-w>k", opts)
+keymap.set("n", "<C-l>", "<C-w>l", default_options)
+keymap.set("n", "<C-h>", "<C-w>h", default_options)
+keymap.set("n", "<C-j>", "<C-w>j", default_options)
+keymap.set("n", "<C-k>", "<C-w>k", default_options)
 
 -- Redo with opposite of undo
-keymap.set("n", "U", "<C-r>", opts)
+keymap.set("n", "U", "<C-r>", default_options)
 
 -- Clear search highlight
-keymap.set("n", "<leader>/", "<cmd>noh<CR>", { desc = "Clear search highlights" })
+keymap.set("n", "<leader>/", "<cmd>noh<CR>", options({ desc = "Clear search highlights" }))
 
 -- Move current line / block with Alt-u/i ala vscode.
 -- Note: not using Alt-j/k because they're used by Zellij
-keymap.set("n", "<A-u>", ":m .+1<CR>==", opts)
-keymap.set("n", "<A-i>", ":m .-2<CR>==", opts)
-keymap.set("i", "<A-u>", "<Esc>:m .+1<CR>==gi", opts)
-keymap.set("i", "<A-i>", "<Esc>:m .-2<CR>==gi", opts)
-keymap.set("v", "<A-u>", ":m '>+1<CR>gv-gv", opts)
-keymap.set("v", "<A-i>", ":m '<-2<CR>gv-gv", opts)
+keymap.set("n", "<A-u>", ":m .+1<CR>==", default_options)
+keymap.set("n", "<A-i>", ":m .-2<CR>==", default_options)
+keymap.set("i", "<A-u>", "<Esc>:m .+1<CR>==gi", default_options)
+keymap.set("i", "<A-i>", "<Esc>:m .-2<CR>==gi", default_options)
+keymap.set("v", "<A-u>", ":m '>+1<CR>gv-gv", default_options)
+keymap.set("v", "<A-i>", ":m '<-2<CR>gv-gv", default_options)
 
 -- Better indenting
-keymap.set("v", "<", "<gv", opts)
-keymap.set("v", ">", ">gv", opts)
+keymap.set("v", "<", "<gv", default_options)
+keymap.set("v", ">", ">gv", default_options)
 
 -- [[ Splits ]]
--- keymap.set("n", "<leader>sv", vim.cmd.vsplit, opts)
--- keymap.set("n", "<leader>sh", vim.cmd.split, opts)
--- open splits with new buffers
-keymap.set("n", "-ss", ":new<cr>", opts)
-keymap.set("n", "-vv", ":vnew<cr>", opts)
-keymap.set("n", "-tb", ":tabnew %<cr>", opts) -- open current buffer in new tab
-keymap.set("n", "-tt", ":tabnew<cr>", opts)
+keymap.set("n", "<leader>sv", vim.cmd.vsplit, options({ desc = "Split vertically" }))
+keymap.set("n", "<leader>sh", vim.cmd.split, options({ desc = "Split horizontally" }))
+keymap.set("n", "-ss", ":new<cr>", options({ desc = "Open split with new buffer" }))
+keymap.set("n", "-vv", ":vnew<cr>", options({ desc = "Open vertical split with new buffer" }))
+keymap.set("n", "-tb", ":tabnew %<cr>", options({ desc = "Open current buffer in new tab" }))
+keymap.set("n", "-tt", ":tabnew<cr>", options({ desc = "Open new tab" }))
 
 -- Commenting - Ctrl-/ to toggle comments in normal/visual mode.
-keymap.set("n", "<C-_>", "<Plug>(comment_toggle_linewise_current)", opts)
-keymap.set("x", "<C-_>", "<Plug>(comment_toggle_linewise_visual)gv", opts)
+keymap.set("n", "<C-_>", "<Plug>(comment_toggle_linewise_current)", default_options)
+keymap.set("x", "<C-_>", "<Plug>(comment_toggle_linewise_visual)gv", default_options)
 
 -- Reformat code
-vim.keymap.set({ "n", "v" }, "<leader>rf", "<cmd>lua vim.lsp.buf.format({async=true})<CR>")
+vim.keymap.set(
+  { "n", "v" },
+  "<leader>rf",
+  "<cmd>lua vim.lsp.buf.format({async=true})<CR>",
+  options({ desc = "Reformat code" })
+)
 
 -- Resize splits with ctrl keys
-keymap.set("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap.set("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap.set("n", "<C-Up>", ":resize -2<CR>", default_options)
+keymap.set("n", "<C-Down>", ":resize +2<CR>", default_options)
+keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", default_options)
+keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", default_options)
 
 -- Copy the current line or visual selection down
-keymap.set("n", "<leader>d", ":copy .<cr>", opts)
-keymap.set("n", "<C-d>", ":copy .<cr>", opts)
-keymap.set("v", "<leader>d", ":copy '<,'>.<CR>", opts)
-keymap.set("v", "<C-d>", ":copy '<,'>.<CR>", opts)
+keymap.set("n", "<leader>yp", ":copy .<cr>", options({ desc = "Yank and paste line" }))
+keymap.set("n", "<C-d>", ":copy .<cr>", default_options)
+keymap.set("v", "<leader>yp", ":copy '<,'>.<CR>", options({ desc = "Yank and paste selection" }))
+keymap.set("v", "<C-d>", ":copy '<,'>.<CR>", default_options)
