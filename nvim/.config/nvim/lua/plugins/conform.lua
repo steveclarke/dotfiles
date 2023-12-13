3cb5c45a004907065e46d4e3492008ffef1b2a6d
Steve Clarke <steve@sevenview.ca>
2023-12-13 12:51:12 -0330

    ctrl-/ to toggle comments

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,34 +1,30 @@
+-- :ConformInfo to view configured and available formatters, as well as log file
+
+local config = function()
+  local conform = require("conform")
+
+  conform.setup({
+    formatters_by_ft = {
+      javascript = { "prettier" },
+      json = { "prettier" },
+      -- ruby = { "standardrb" }, --currently broken, see autocmds.lua for workaround
+      vue = { "prettier" },
+      lua = { "stylua" },
+      markdown = { "markdownlint" },
+    },
+
+    format_on_save = {
+      lsp_fallback = true,
+      async = false,
+      timeout_ms = 500,
+    },
+  })
+end
+
 return {
   "stevearc/conform.nvim",
   enabled = true,
-  event = { "BufRead", "BufNewFile" },
-  config = function()
-    local conform = require("conform")
-
-    conform.setup({
-      formatters_by_ft = {
-        javascript = { "prettier" },
-        json = { "prettier" },
-        -- ruby = { "standardrb" }, --currently broken, see autocmds.lua for workaround
-        vue = { "prettier" },
-        lua = { "stylua" },
-        markdown = { "markdownlint" },
-      },
-
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 500,
-      },
-    })
-
-    -- FIXME: This doesn't work properly in visual mode
-    -- vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-    --   conform.format({
-    --     lsp_fallback = true,
-    --     async = false,
-    --     timeout_ms = 500,
-    --   })
-    -- end, { desc = "Format file or range" })
-  end,
+  event = { "BufWritePre" },
+  config = config,
+  cmd = { "ConformInfo" },
 }
