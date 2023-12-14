5b4c276f99b7a1e88d8863533c2d7912b2192be1
Steve Clarke <steve@sevenview.ca>
2023-12-14 10:40:24 -0330

    tidy direnv config

--- fish/.config/fish/conf.d/direnv.fish
+++ fish/.config/fish/conf.d/direnv.fish
@@ -3,11 +3,9 @@
 # https://lifesaver.codes/answer/fish-direnv-is-no-longer-triggered-on-shell-creation-583
 # This solves a problem where the .envrc wasn't parsed immediately after
 # VSCode opens a new terminal instance.
-if status is-interactive
-  if command -q direnv
-    direnv hook fish | source
-    direnv export fish | source
-  else
-    echo "direnv not found. Install with 'apt install direnv'"
-  end
+if status is-interactive && command -q direnv
+  direnv hook fish | source
+  direnv export fish | source
+else
+  echo "direnv not found. Install with 'apt install direnv'"
 end
