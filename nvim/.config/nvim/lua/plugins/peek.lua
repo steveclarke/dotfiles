ad1b7194df880b4c9d29f44fb2cbb2b9b54c405d
Steve Clarke <steve@sevenview.ca>
2023-12-16 11:29:09 -0330

    nvim: add peek plugin to preview markdown

---
+++ nvim/.config/nvim/lua/plugins/peek.lua
@@ -0,0 +1,11 @@
+return {
+  "toppair/peek.nvim",
+  event = { "VeryLazy" },
+  build = "deno task --quiet build:fast",
+  config = function()
+    local peek = require("peek")
+    peek.setup()
+    vim.api.nvim_create_user_command("PeekOpen", peek.open, {})
+    vim.api.nvim_create_user_command("PeekClose", peek.close, {})
+  end,
+}
