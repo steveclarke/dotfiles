20390a7d400d1d60b43de0b5b1075e03ba8a29a6
Steve Clarke <steve@sevenview.ca>
2023-12-15 22:31:24 -0330

    nvim: fish files indent 4

--- nvim/.config/nvim/lua/core/autocmds.lua
+++ nvim/.config/nvim/lua/core/autocmds.lua
@@ -1,43 +1,44 @@
--- [[ Highlight on yank ]]
+-- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 
-vim.api.nvim_create_autocmd(
-  "TextYankPost",
-  {
-    group = highlight_group,
-    pattern = "*",
-    callback = function()
-      vim.highlight.on_yank()
-    end,
-  }
-)
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = highlight_group,
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
 
--- standardrb
-vim.api.nvim_create_autocmd(
-  "FileType", {
-    pattern = "ruby",
-    group = vim.api.nvim_create_augroup("RubyLSP", { clear = true }),
-    callback = function()
-      vim.lsp.start {
-        name = "standard",
-        cmd = { "standardrb", "--lsp" },
-        on_attach = function()
-          --
-        end,
-      }
-    end,
-  }
-)
+-- StandardRb LSP for Ruby files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "ruby",
+  group = vim.api.nvim_create_augroup("RubyLSP", { clear = true }),
+  callback = function()
+    vim.lsp.start({
+      name = "standard",
+      cmd = { "standardrb", "--lsp" },
+      on_attach = function() end,
+    })
+  end,
+})
 
--- automatically format ruby code on save
-vim.api.nvim_create_autocmd(
-  "BufWrite",
-  {
-    pattern = "*.rb, *.jbuilder, *.rake, Gemfile, Rakefile, *.gemspec, *.ru, *.erb",
-    group = vim.api.nvim_create_augroup("RubyFormat", { clear = true }),
-    callback = function()
-      vim.lsp.buf.format({async = true})
-    end,
-  }
-)
+-- Automatically format ruby code on save
+vim.api.nvim_create_autocmd("BufWrite", {
+  pattern = "*.rb, *.jbuilder, *.rake, Gemfile, Rakefile, *.gemspec, *.ru, *.erb",
+  group = vim.api.nvim_create_augroup("RubyFormat", { clear = true }),
+  callback = function()
+    vim.lsp.buf.format({ async = true })
+  end,
+})
 
+-- 4 spaces for fish files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "fish",
+  group = vim.api.nvim_create_augroup("FishIndent", { clear = true }),
+  callback = function()
+    vim.bo.tabstop = 4
+    vim.bo.softtabstop = 4
+    vim.bo.shiftwidth = 4
+    vim.bo.expandtab = true
+  end,
+})
