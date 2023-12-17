2b67785085bd5828bb10454d2b09aa5bf9615aef
Steve Clarke <steve@sevenview.ca>
2023-12-16 23:06:23 -0330

    nvim: add keymaps for buffers

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -5,11 +5,17 @@ local options = require("utils.core").keymap_options
 keymap.set("n", "<leader>e", vim.cmd.NvimTreeFocus, options({ desc = "Focus file explorer" }))
 keymap.set("n", "<leader>fe", vim.cmd.NvimTreeToggle, options({ desc = "Toggle file explorer" }))
 
--- Map Ctrl-l to move to the window on the right
+-- Move to windows using <ctrl> hjkl
 keymap.set("n", "<C-l>", "<C-w>l", options())
 keymap.set("n", "<C-h>", "<C-w>h", options())
 keymap.set("n", "<C-j>", "<C-w>j", options())
 keymap.set("n", "<C-k>", "<C-w>k", options())
+
+-- Resize window with <ctrl> arrow keys
+keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", options({ desc = "Increase window height" }))
+keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", options({ desc = "Decrease window height" }))
+keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", options({ desc = "Decrease window width" }))
+keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", options({ desc = "Increase window width" }))
 
 -- Redo with opposite of undo
 keymap.set("n", "U", "<C-r>", options())
@@ -17,18 +23,26 @@ keymap.set("n", "U", "<C-r>", options())
 -- Clear search highlight
 keymap.set("n", "<leader>/", "<cmd>noh<CR>", options({ desc = "Clear search highlights" }))
 
--- Move current line / block with Alt-u/i ala vscode.
+-- Move current line / block with <alt> u/i ala vscode.
 -- Note: not using Alt-j/k because they're used by Zellij
-keymap.set("n", "<A-u>", ":m .+1<CR>==", options())
-keymap.set("n", "<A-i>", ":m .-2<CR>==", options())
-keymap.set("i", "<A-u>", "<Esc>:m .+1<CR>==gi", options())
-keymap.set("i", "<A-i>", "<Esc>:m .-2<CR>==gi", options())
-keymap.set("v", "<A-u>", ":m '>+1<CR>gv-gv", options())
-keymap.set("v", "<A-i>", ":m '<-2<CR>gv-gv", options())
+keymap.set("n", "<A-u>", "<cmd>m .+1<cr>==", options())
+keymap.set("n", "<A-i>", "<cmd>m .-2<cr>==", options())
+keymap.set("i", "<A-u>", "<esc><cmd>m .+1<cr>==gi", options())
+keymap.set("i", "<A-i>", "<esc><cmd>m .-2<cr>==gi", options())
+keymap.set("v", "<A-u>", "<cmd>m '>+1<cr>gv=gv", options())
+keymap.set("v", "<A-i>", "<cmd>m '<-2<cr>gv=gv", options())
 
 -- Better indenting
 keymap.set("v", "<", "<gv", options())
 keymap.set("v", ">", ">gv", options())
+
+-- Buffers
+keymap.set("n", "<S-h>", "<cmd>bprevious<cr>", options({ desc = "Prev buffer" }))
+keymap.set("n", "<S-l>", "<cmd>bnext<cr>", options({ desc = "Next buffer" }))
+keymap.set("n", "[b", "<cmd>bprevious<cr>", options({ desc = "Prev buffer" }))
+keymap.set("n", "]b", "<cmd>bnext<cr>", options({ desc = "Next buffer" }))
+keymap.set("n", "<leader>bb", "<cmd>e #<cr>", options({ desc = "Switch to Other Buffer" }))
+keymap.set("n", "<leader>`", "<cmd>e #<cr>", options({ desc = "Switch to Other Buffer" }))
 
 -- [[ Splits ]]
 keymap.set("n", ",s", ":new<cr>", options({ desc = "Open split with new buffer" }))
@@ -47,12 +61,6 @@ vim.keymap.set(
   "<cmd>lua vim.lsp.buf.format({async=true})<CR>",
   options({ desc = "Reformat code" })
 )
-
--- Resize splits with ctrl keys
-keymap.set("n", "<C-Up>", ":resize -2<CR>", options())
-keymap.set("n", "<C-Down>", ":resize +2<CR>", options())
-keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", options())
-keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", options())
 
 -- Copy the current line or visual selection down
 keymap.set("n", "<leader>yp", ":copy .<cr>", options({ desc = "Yank and paste line" }))
