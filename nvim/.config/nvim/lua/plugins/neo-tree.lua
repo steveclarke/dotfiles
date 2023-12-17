d2b8afb29f3e0f97da8052b61ec542ea41631892
Steve Clarke <steve@sevenview.ca>
2023-12-17 13:19:07 -0330

    nvim: neo-tree

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -2,10 +2,17 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
   cmd = "Neotree",
+  lazy = false, -- load so it takes over netrw when `nvim .` is invoked
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  opts = {
+    filesystem = {
+      hijack_netrw_behavior = "open_current",
+      follow_current_file = { enabled = true },
+    },
   },
 }
