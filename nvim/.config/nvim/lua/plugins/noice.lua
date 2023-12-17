e62fa53a6ed6ad120096a36ac00415eb0adf6ff0
Steve Clarke <steve@sevenview.ca>
2023-12-17 19:48:20 -0330

    nvim: re-enable Noice

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -1,17 +1,49 @@
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
-  enabled = false,
+  enabled = true,
   dependencies = {
     "munifTanjim/nui.nvim",
     "rcarriga/nvim-notify",
   },
   opts = {
-    routes = {
-      {
-        view = "notify",
-        filter = { event = "msg_showmode" },
+    lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
       },
     },
+    -- Note: @recording messages are shown in the lualine (see lualine.lua)
+    routes = {
+      {
+        filter = {
+          event = "msg_show",
+          any = {
+            { find = "%d+L, %d+B" },
+            { find = "; after #%d+" },
+            { find = "; before #%d+" },
+          },
+        },
+        view = "mini",
+      },
+    },
+    presets = {
+      bottom_search = true, -- use a classic bottom cmdline for search
+      command_palette = true, -- position the cmdline and popupmenu together
+      long_message_to_split = true, -- long messages will be sent to a split
+      inc_rename = true, -- enables an input dialog for inc-rename.nvim
+      lsp_doc_border = false, -- add a border to hover docs and signature help
+    },
   },
+  -- stylua: ignore
+  --  keys = {
+  --    { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
+  --    { "<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
+  --    { "<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History" },
+  --    { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
+  --    { "<leader>snd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
+  --    { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll forward", mode = {"i", "n", "s"} },
+  --    { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll backward", mode = {"i", "n", "s"}},
+  -- },
 }
