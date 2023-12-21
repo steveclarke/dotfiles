7a49593f70b98805953c483b0045ea4f474a3624
Steve Clarke <steve@sevenview.ca>
2023-12-21 11:58:24 -0330

    nvim: add window-picker plugin

--- nvim/.config/nvim/lua/plugins/daily-focus.lua
+++ nvim/.config/nvim/lua/plugins/daily-focus.lua
@@ -1,6 +1,7 @@
 return {
-  dir = "/home/steve/src/daily-focus.nvim",
-  enabled = false,
+  "steveclarke/daily-focus.nvim",
+  -- dir = "/home/steve/src/daily-focus.nvim",
+  enabled = true,
   -- dev = true,
   -- lazy = false
   opts = {},
@@ -8,5 +9,5 @@ return {
     local daily_focus = require("daily-focus")
     daily_focus.setup({ data_path = "/foo/bar" })
     vim.print(daily_focus.tip())
-  end
+  end,
 }
