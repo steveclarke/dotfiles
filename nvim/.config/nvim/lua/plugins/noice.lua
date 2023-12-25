770625bcc23c88fa0924b1aac1fe78b8da78244a
Steve Clarke <steve@sevenview.ca>
2023-12-24 22:38:55 -0330

    nvim: fix checkhealth plugin repo name warning

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -3,7 +3,7 @@ return {
   event = "VeryLazy",
   enabled = true,
   dependencies = {
-    "munifTanjim/nui.nvim",
+    "MunifTanjim/nui.nvim",
     "rcarriga/nvim-notify",
   },
   opts = {
@@ -29,11 +29,11 @@ return {
       },
     },
     presets = {
-      bottom_search = false, -- use a classic bottom cmdline for search
-      command_palette = true, -- position the cmdline and popupmenu together
+      bottom_search = false,        -- use a classic bottom cmdline for search
+      command_palette = true,       -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
-      inc_rename = true, -- enables an input dialog for inc-rename.nvim
-      lsp_doc_border = false, -- add a border to hover docs and signature help
+      inc_rename = true,            -- enables an input dialog for inc-rename.nvim
+      lsp_doc_border = false,       -- add a border to hover docs and signature help
     },
   },
   -- stylua: ignore
