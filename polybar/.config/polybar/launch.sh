3dea50506d9b0ce7a52cae397548eb629cb3d0b9
Steve Clarke <steve@sevenview.ca>
2023-12-30 11:55:04 -0330

    i3: add i3 config to setup scripts (wip)

---
+++ polybar/.config/polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+polybar-msg cmd quit
+
+# TODO: Configure multiple monitors
+# https://github.com/polybar/polybar/issues/763
+# for m in $(polybar --list-monitors | cut -d":" -f1); do
+# 	MONITOR=$m polybar --reload example &
+# done
+
+polybar --reload example &
