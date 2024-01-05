34b2ec2edc6beb367f7621f153eeaa4ea14f0237
Steve Clarke <steve@sevenview.ca>
2024-01-05 14:55:02 -0330

    nvim: Add bbye plugin to delete buffers, preserving windows

---
+++ nvim/.config/nvim/lua/plugins/vim-bbye.lua
@@ -0,0 +1,7 @@
+return {
+  "moll/vim-bbye",
+  keys = {
+    { "<leader>bd", ":Bdelete<CR>", desc = "Delete buffer (preserving windows)" },
+    { "<leader>bw", ":Bwipeout<CR>", desc = "Wipeout buffer (preserving windows)" },
+  },
+}
