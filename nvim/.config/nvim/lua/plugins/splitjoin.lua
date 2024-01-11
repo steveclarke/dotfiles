476200daf32fac2021a0ef003ecc387ed78f09de
Steve Clarke <steve@sevenview.ca>
2024-01-11 09:54:35 -0330

    nvim: add splitjoin plugin

---
+++ nvim/.config/nvim/lua/plugins/splitjoin.lua
@@ -0,0 +1,6 @@
+-- gS to split a one-liner into multiple lines
+-- gJ (with the cursor on the first line of a block) to join a block into a single-line statement.
+-- alternative is https://github.com/Wansmer/treesj with treesitter support
+return {
+  "AndrewRadev/splitjoin.vim",
+}
