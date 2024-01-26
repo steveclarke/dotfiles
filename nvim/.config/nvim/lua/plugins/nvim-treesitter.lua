f173ce8c59408352086d3e5e8f9f55dbffec3880
Steve Clarke <steve@sevenview.ca>
2024-01-26 09:13:47 -0330

    nvim: add Oil plugin

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -58,7 +58,7 @@ end
 return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects"
+    "nvim-treesitter/nvim-treesitter-textobjects",
   },
   lazy = false,
   build = ":TSUpdate",
