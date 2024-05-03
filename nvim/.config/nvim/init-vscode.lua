bbd0c381ed50b573e27c739f21a255bba20a8212
Steve Clarke <steve@sevenview.ca>
2024-05-02 22:41:16 -0230

    updates

--- nvim/.config/nvim/init-vscode.lua
+++ nvim/.config/nvim/init-vscode.lua
@@ -24,3 +24,21 @@ require("lazy").setup({
     end,
   },
 }, lazy_opts)
+
+local keymap = vim.keymap
+
+-- Redo with opposite of undo
+keymap.set("n", "U", "<C-r>")
+
+-- Move current line / block with <alt> j/k
+-- keymap.set("n", "<A-k>", "<cmd>m .+1<cr>==")
+-- keymap.set("n", "<A-j>", "<cmd>m .-2<cr>==")
+-- keymap.set("i", "<A-u>", "<esc><cmd>m .+1<cr>==gi")
+-- keymap.set("i", "<A-i>", "<esc><cmd>m .-2<cr>==gi")
+-- keymap.set("v", "<A-u>", "<cmd>m '>+1<cr>gv=gv")
+-- keymap.set("v", "<A-i>", "<cmd>m '<-2<cr>gv=gv")
+
+-- Better indenting
+keymap.set("v", "<", "<gv")
+keymap.set("v", ">", ">gv")
+
