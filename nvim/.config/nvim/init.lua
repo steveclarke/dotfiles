9fc99af581e5307152b2c3ade05cbfa3fa2e120e
Steve Clarke <steve@sevenview.ca>
2024-05-05 19:30:30 -0230

    new neovim config that's vscode-aware

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,68 @@
-print('foo')
+local vscode = vim.g.vscode
+
+--
+-- [ PLUGINS ]
+--
+local lazypath = vim.fn.stdpath("data") .. "lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- NOTE: Add enabled = not vscode to skip loading a plugin if in vscode
+require("lazy").setup({
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    config = function()
+      require("nvim-surround").setup({})
+    end
+  }
+})
+
+-- <leader> key is <space>
+vim.g.mapleader = " "
+
+-- Open init.lua with <leader>i
+vim.cmd("nmap <leader>i :e ~/.config/nvim/init.lua<cr>")
+
+--
+-- [ OPTIONS ]
+--
+
+-- Use system clipboard
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+--
+-- [ KEYMAPS ]
+--
+
+-- Redo with opposite of undo
+vim.keymap.set("n", "U", "<C-r>")
+
+-- Clear search highlights
+vim.keymap.set("n", "<leader>/", "<cmd>nohlsearch<cr>")
+
+-- Better indenting
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+-- Highlight on yank
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = highlight_group,
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
