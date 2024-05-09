0701a2b6dd32c05f35a7e9d06912d53f1d07672c
Steve Clarke <steve@sevenview.ca>
2024-05-09 08:39:14 -0230

    nvim: add surround

--- nvim/.config/nvim/lua/plugins/surround.lua
+++ nvim/.config/nvim/lua/plugins/surround.lua
@@ -4,9 +4,16 @@ return {
   config = function()
     require("nvim-surround").setup({
       surrounds = {
+        -- (h)andlebar tags
         ["h"] = {
           add = function()
             return  { { "{{ " }, { " }}" } }
+          end,
+        },
+        -- Markdown (c)ode block
+        ["c"] = {
+          add = function()
+            return  { { "```" }, { "```" } }
           end,
         }
       }
