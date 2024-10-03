515644ec0e71d82e3259799fe0338b0422f339db
Steve <steve@sevenview.ca>
2024-10-03 17:12:49 -0230

    Add Jetbrains Toolbox and AppImage pre-reqs

---
+++ install/desktop/app-jetbrains-toolbox.sh
@@ -0,0 +1,6 @@
+# Install AppImage prerequisites
+sh ./2-appimage.sh
+
+# https://github.com/nagygergo/jetbrains-toolbox-install
+curl -fsSL https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install/master/jetbrains-toolbox.sh | bash
+
