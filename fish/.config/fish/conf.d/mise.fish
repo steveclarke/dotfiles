90ca7927e9220f30daf3482ad03f981aab45bb8b
Steve <steve@sevenview.ca>
2024-10-04 17:09:40 -0230

    add more apps and configs

---
+++ fish/.config/fish/conf.d/mise.fish
@@ -0,0 +1,7 @@
+# Mise
+# https://mise.jdx.dev/getting-started.html 
+
+if test -f ~/.local/bin/mise
+  ~/.local/bin/mise activate fish | source
+  #~/.local/bin/mise activate fish --shims | source
+end
---
+++ install.sh
@@ -0,0 +1,4 @@
+# This is the temporary holding place for the new core install experience.
+
+for installer in ~/dotfiles/install/prereq/*.sh; do source $installer; done
+sh ~/dotfiles/install/desktop.sh
