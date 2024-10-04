f4355d7aac332f30ce7edacc22d53093280e3364
Steve <steve@sevenview.ca>
2024-10-04 18:41:53 -0230

    Ring Central, Slack

--- install/apps/slack.sh
+++ install/apps/slack.sh
@@ -1,1 +1,16 @@
 flatpak install -y flathub com.slack.Slack
+
+autostart_dir="$HOME/.config/autostart"
+
+cat <<EOL > "$autostart_dir"/slack.desktop
+[Desktop Entry]
+Type=Application
+Exec=flatpak run com.slack.Slack
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name=Slack
+Comment=Start Slack on login
+EOL
+
+chmod +x "$autostart_dir"/slack.desktop
