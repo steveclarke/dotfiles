c51f09196223aa8fe49452e0ed40f7afb6447d8f
Steve <steve@sevenview.ca>
2024-10-04 20:24:54 -0230

    parcel desktop entry

---
+++ install/desktop-entries/parcel.sh
@@ -0,0 +1,13 @@
+cat <<EOF >~/.local/share/applications/Parcel.desktop
+[Desktop Entry]
+Version=1.0
+Name=Parcel
+Comment=Parcel tracking app
+Exec=google-chrome --app="https://web.parcelapp.net" --name=Parcel --class=Parcel
+Terminal=false
+Type=Application
+Icon=/home/$USER/dotfiles/install/desktop-entries/icons/Parcel.png
+Categories=GTK;
+MimeType=text/html;text/xml;application/xhtml_xml;
+StartupNotify=true
+EOF
