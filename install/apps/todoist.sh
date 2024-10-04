b8bd15b3e32fac9a65145bb22c8cc1e644f72435
Steve <steve@sevenview.ca>
2024-10-04 18:43:52 -0230

    autostart todoist

--- install/apps/todoist.sh
+++ install/apps/todoist.sh
@@ -1,2 +1,16 @@
 sudo snap install todoist
  
+autostart_dir="$HOME/.config/autostart"
+
+cat <<EOL > "$autostart_dir"/todoist.desktop
+[Desktop Entry]
+Type=Application
+Exec=todoist
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name=Todoist
+Comment=Start Todoist on login
+EOL
+
+chmod +x "$autostart_dir"/todoist.desktop
