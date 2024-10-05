4577fb28df6ee0ed1e77a41f80da001c76b4f3f0
Steve <steve@sevenview.ca>
2024-10-05 15:23:14 -0230

    docker install, git config, default libs

--- setups/git.sh
+++ setups/git.sh
@@ -1,3 +1,5 @@
 git config --global color.ui true
 git config --global user.name "${DOTFILES_GIT_USER_NAME}"
 git config --global user.email "${DOTFILES_GIT_USER_EMAIL}"
+git config --global pull.rebase false
+git config --global init.defaultBranch master
