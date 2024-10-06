6dc0f2f913ebe3c1d395e5a59e695e1d35bb42b9
Steve <steve@sevenview.ca>
2024-10-06 19:04:41 -0230

    add Gnome extensions config

---
+++ setups/gnome-extensions.sh
@@ -0,0 +1,34 @@
+# Install pipx (python package manager) and gnome-extensions-cli
+sudo apt install -y pipx
+pipx install gnome-extensions-cli --system-site-packages
+
+# Disable default extensions
+gnome-extensions disable ding@rastersoft.com
+
+# Install new extensions
+gext install tactile@lundal.io
+gext install vertical-workspaces@G-dH.github.com
+gext install tophat@fflewddur.github.io
+gext install azclock@azclock.gitlab.com
+gext install clipboard-history@alexsaveau.dev
+
+# Compile gsettings schemas in order to be able to set them
+sudo cp ~/.local/share/gnome-shell/extensions/tactile@lundal.io/schemas/org.gnome.shell.extensions.tactile.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/tophat@fflewddur.github.io/schemas/org.gnome.shell.extensions.tophat.gschema.xml /usr/share/glib-2.0/schemas/
+sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
+
+# Configure Tactile
+gsettings set org.gnome.shell.extensions.tactile col-0 1
+gsettings set org.gnome.shell.extensions.tactile col-1 2
+gsettings set org.gnome.shell.extensions.tactile col-2 1
+gsettings set org.gnome.shell.extensions.tactile col-3 0
+gsettings set org.gnome.shell.extensions.tactile row-0 1
+gsettings set org.gnome.shell.extensions.tactile row-1 1
+gsettings set org.gnome.shell.extensions.tactile gap-size 32
+
+# Configure TopHat
+gsettings set org.gnome.shell.extensions.tophat show-icons false
+gsettings set org.gnome.shell.extensions.tophat show-cpu false
+gsettings set org.gnome.shell.extensions.tophat show-disk false
+gsettings set org.gnome.shell.extensions.tophat show-mem false
+gsettings set org.gnome.shell.extensions.tophat network-usage-unit bits
