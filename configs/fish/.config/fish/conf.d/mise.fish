5a36c18e1d13620747d366e570b521ba3ddab62d
Steve <steve@sevenview.ca>
2024-10-06 14:16:49 -0230

    add a fix for Mise running under vscode. use shims when so

--- configs/fish/.config/fish/conf.d/mise.fish
+++ configs/fish/.config/fish/conf.d/mise.fish
@@ -2,6 +2,12 @@
 # https://mise.jdx.dev/getting-started.html 
 
 if test -f ~/.local/bin/mise
-  ~/.local/bin/mise activate fish | source
-  #~/.local/bin/mise activate fish --shims | source
+    # https://github.com/jdx/mise/issues/2270#issuecomment-2211805443
+    if test "$VSCODE_RESOLVING_ENVIRONMENT" = 1
+        mise activate fish --shims | source
+    else if status is-interactive
+        mise activate fish | source
+    else
+        mise activate fish --shims | source
+    end
 end
