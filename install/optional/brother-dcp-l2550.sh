7eee17604ac51ffa32db4705c33fc40554860e2c
Steve <steve@sevenview.ca>
2024-10-06 14:40:01 -0230

    another printer driver

---
+++ install/optional/brother-dcp-l2550.sh
@@ -0,0 +1,10 @@
+# https://support.brother.com/g/b/downloadhowto.aspx?c=ca&lang=en&prod=dcpl2550dw_us&os=128&dlid=dlf103577_000&flang=4&type3=10283
+installer_file="$(find "${HOME}"/Downloads -name "dcpl2550*.deb")"
+
+if [ -z "$installer_file" ]; then
+  echo "Brother installer file not found in Downloads folder"
+  exit 1
+fi
+
+echo "Installing $installer_file"
+sudo apt install -y "$installer_file"
