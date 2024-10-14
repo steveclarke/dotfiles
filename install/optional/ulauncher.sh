a41b8ed029b1a630232474fd1f0550b19f3c71fc
Steve <steve@sevenview.ca>
2024-10-14 13:49:04 -0230

    Gnome settings (wip); ulauncher

---
+++ install/optional/ulauncher.sh
@@ -0,0 +1,4 @@
+sudo add-apt-repository universe -y
+sudo add-apt-repository ppa:agornostal/ulauncher -y
+sudo apt update
+sudo apt install -y ulauncher
---
+++ setups/gnome-hotkeys.sh
@@ -0,0 +1,7 @@
+# TODO: These don't seem to work on Ubuntu 24.04. Seetting them manually in the
+# settings GUI works for now.
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys terminal "['<Super>e']"
