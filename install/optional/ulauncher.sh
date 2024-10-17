3910ce322d95ae9414cdb66c748db964146ef13f
Steve <steve@sevenview.ca>
2024-10-17 08:47:23 -0230

    ulauncher notes

--- install/optional/ulauncher.sh
+++ install/optional/ulauncher.sh
@@ -2,3 +2,6 @@ sudo add-apt-repository universe -y
 sudo add-apt-repository ppa:agornostal/ulauncher -y
 sudo apt update
 sudo apt install -y ulauncher
+
+# Lots of quirks with this one under Wayland.
+# https://github.com/Ulauncher/Ulauncher/discussions/1350
