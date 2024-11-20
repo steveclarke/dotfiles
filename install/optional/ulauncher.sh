813482aef180b18412c52465f94bf49147d74f70
Steve <steve@sevenview.ca>
2024-11-20 08:39:28 -0330

    add note on disabling ulauncher default hotkey since this doesn't work from the gui settings

--- install/optional/ulauncher.sh
+++ install/optional/ulauncher.sh
@@ -5,3 +5,7 @@ sudo apt install -y ulauncher
 
 # Lots of quirks with this one under Wayland.
 # https://github.com/Ulauncher/Ulauncher/discussions/1350
+
+# To disable the ulauncher default shortcut on Wayland, edit:
+# .config/ulauncher/settings.json
+# Set "hotkey-show-app": "null"
