60447676dad4b8ceb2cf6cf92f4dc4f1f17084e0
Steve <steve@sevenview.ca>
2024-11-20 08:39:06 -0330

    trying to make lofree run once on graphical login

--- setups/lofree.sh
+++ setups/lofree.sh
@@ -1,3 +1,5 @@
+source "${HOME}"/.dotfilesrc
+
 # The Lofree keyboard I'm using has a quirk when in Windows mode such that the
 # function keys don't work. They only work as media keys.  The solution seems to
 # be to switch to Mac mode, but that causes the Alt and Windows keys to be
@@ -14,3 +16,25 @@
 echo 1 | sudo tee /sys/module/hid_apple/parameters/fnmode
 
 echo 1 | sudo tee /sys/module/hid_apple/parameters/swap_opt_cmd
+
+# Configure the system to run this script on startup
+# service_file="/etc/systemd/system/lofree.service"
+
+# if [ ! -f "$service_file" ]; then
+
+#   cat <<EOL | sudo tee  "$service_file"
+# [Unit]
+# Description=Swap Lofree keyboard keys
+
+# [Service]
+# ExecStart=${DOTFILES_DIR}/setups/lofree.sh
+# Type=oneshot
+# RemainAfterExit=true
+
+# [Install]
+# WantedBy=default.target
+# EOL
+
+#   # sudo cp "$service_file" /etc/systemd/system/lofree.service
+#   sudo systemctl enable lofree
+# fi
