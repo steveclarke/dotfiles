60eec37d444607d3768eb2c8fe57b85ebfc0cc86
Steve <steve@sevenview.ca>
2025-01-27 11:32:28 -0330

    fix bug in vscode term detection

--- configs/fish/.config/fish/conf.d/zellij.fish
+++ configs/fish/.config/fish/conf.d/zellij.fish
@@ -2,7 +2,9 @@
 
 # Automatically open and exit Zellij when the shell is interactive, but ignore
 # VSCode.
-if status is-interactive  and test "$VSCODE_RESOLVING_ENVIRONMENT" != 1
-  set ZELLIJ_AUTO_EXIT true
-  eval (zellij setup --generate-auto-start fish | string collect)
+if status is-interactive
+    if test "$TERM_PROGRAM" != "vscode"
+        set ZELLIJ_AUTO_EXIT true
+        eval (zellij setup --generate-auto-start fish | string collect)
+    end
 end
