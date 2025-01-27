172356774705c4b75aff1f9227d7d38a1f7d1c21
Steve <steve@sevenview.ca>
2025-01-27 09:30:41 -0330

    don't zellij in vscode

--- configs/fish/.config/fish/conf.d/zellij.fish
+++ configs/fish/.config/fish/conf.d/zellij.fish
@@ -1,6 +1,8 @@
 # https://zellij.dev/documentation/integration
 
-if status is-interactive
+# Automatically open and exit Zellij when the shell is interactive, but ignore
+# VSCode.
+if status is-interactive  and test "$VSCODE_RESOLVING_ENVIRONMENT" != 1
   set ZELLIJ_AUTO_EXIT true
   eval (zellij setup --generate-auto-start fish | string collect)
 end
