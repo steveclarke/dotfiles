9fe3bd07a3553afe0d04b6ae3d91321b5ab44a74
Steve <steve@sevenview.ca>
2025-01-28 08:19:20 -0330

    only auto-open/close Zellij when using my main terminal app (Alacritty)

--- configs/fish/.config/fish/conf.d/zellij.fish
+++ configs/fish/.config/fish/conf.d/zellij.fish
@@ -2,9 +2,15 @@
 
 # Automatically open and exit Zellij when the shell is interactive, but ignore
 # VSCode.
-if status is-interactive
-    if test "$TERM_PROGRAM" != "vscode"
-        set ZELLIJ_AUTO_EXIT true
-        eval (zellij setup --generate-auto-start fish | string collect)
-    end
+# if status is-interactive
+#     if test "$TERM_PROGRAM" != "vscode"
+#         set ZELLIJ_AUTO_EXIT true
+#         eval (zellij setup --generate-auto-start fish | string collect)
+#     end
+# end
+
+# Automatically open and exit Zellij when using Alacritty.
+if set -q ALACRITTY_WINDOW_ID
+    set ZELLIJ_AUTO_EXIT true
+    eval (zellij setup --generate-auto-start fish | string collect)
 end
