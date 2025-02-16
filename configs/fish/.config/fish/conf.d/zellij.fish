3b5ab108a371bc793e24e1724ad114538db0e9db
Steve <steve@sevenview.ca>
2025-02-16 19:46:28 -0330

    also run Zellij automatically when in Ghostty

--- configs/fish/.config/fish/conf.d/zellij.fish
+++ configs/fish/.config/fish/conf.d/zellij.fish
@@ -10,7 +10,7 @@
 # end
 
 # Automatically open and exit Zellij when using Alacritty.
-if set -q ALACRITTY_WINDOW_ID
+if set -q ALACRITTY_WINDOW_ID || test $TERM_PROGRAM = ghostty
     set ZELLIJ_AUTO_EXIT true
     eval (zellij setup --generate-auto-start fish | string collect)
 end
