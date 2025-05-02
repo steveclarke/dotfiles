67819160bc2d23eed5f2693f780a2a281dc8c1db
Steve <steve@sevenview.ca>
2025-05-02 13:43:01 -0230

    add gear-lever, update cursor installer

--- install/optional/cursor.sh
+++ install/optional/cursor.sh
@@ -2,10 +2,18 @@
 
 cd /tmp || exit
 curl -L "https://www.cursor.com/api/download?platform=linux-x64&releaseTrack=stable" | jq -r '.downloadUrl' | xargs curl -L -o cursor.appimage
-sudo mv cursor.appimage /opt/cursor.appimage
-sudo chmod +x /opt/cursor.appimage
+sudo mv cursor.appimage /opt/cursor/cursor.appimage
+sudo chmod +x /opt/cursor/cursor.appimage
 sudo apt install -y fuse3
 sudo apt install -y libfuse2t64
+
+ICON_URL="https://registry.npmmirror.com/@lobehub/icons-static-png/latest/files/dark/cursor.png"
+
+# Download Cursor icon
+echo "Downloading Cursor icon..."
+if ! sudo curl -L $ICON_URL -o /opt/cursor/cursor.png; then
+	echo "Failed to download Cursor icon, but continuing installation."
+fi
 
 DESKTOP_FILE="/usr/share/applications/cursor.desktop"
 
@@ -13,7 +21,7 @@ sudo bash -c "cat > $DESKTOP_FILE" <<EOL
 [Desktop Entry]
 Name=Cursor
 Comment=AI-powered code editor
-Exec=/opt/cursor.appimage --no-sandbox --ozone-platform=wayland
+Exec=/opt/cursor/cursor.appimage --ozone-platform=wayland %F
 Icon=/opt/cursor/cursor.png
 Type=Application
 Categories=Development;IDE;
