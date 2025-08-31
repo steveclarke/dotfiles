c1cac83ce40008e014e967776806152c3f47b7f0
Steve Clarke <steve@sevenview.ca>
2025-08-31 14:41:23 -0230

    add Oil plugin

---
+++ configs/nvim-handcrafted/.config/nvim-handcrafted/lua/Steve/plugins/oil.lua
@@ -0,0 +1,15 @@
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {
+    default_file_explorer = true,
+    delete_to_trash = false,
+    view_options = {
+      show_hidden = true,
+    },
+  },
+  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
+}
