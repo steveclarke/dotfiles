ad7502622671f77d16fe1fa249568c2d3d3f16b7
Steve Clarke <steve@sevenview.ca>
2025-09-01 18:58:00 -0230

    Enhance Neovim Treesitter configuration with improved structure and new plugin integration
    - Refactored treesitter.lua for better organization and clarity, maintaining comprehensive comments for configuration options.
    - Added the windwp/nvim-ts-autotag plugin for automatic closing and renaming of HTML/JSX tags, enhancing development efficiency.

--- configs/nvim/.config/nvim-handcrafted/lua/steve/plugins/treesitter.lua
+++ configs/nvim/.config/nvim-handcrafted/lua/steve/plugins/treesitter.lua
@@ -2,61 +2,71 @@
 -- by building abstract syntax trees for different programming languages
 
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  -- Automatically update all installed language parsers when plugin updates
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      -- Automatically install missing parsers when entering buffer
-      auto_install = true,
+  -- Core treesitter plugin
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    -- Automatically update all installed language parsers when plugin updates
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        -- Automatically install missing parsers when entering buffer
+        auto_install = true,
 
-      -- Enable improved syntax highlighting based on treesitter
-      highlight = { enable = true, },
+        -- Enable improved syntax highlighting based on treesitter
+        highlight = { enable = true, },
 
-      indent = { enable = true, },
+        indent = { enable = true, },
 
-      ensure_installed = {
-        "bash",
-        "c",
-        "diff",
-        "html",
-        "javascript",
-        "jsdoc",
-        "json",
-        "jsonc",
-        "lua",
-        "luadoc",
-        "luap",
-        "markdown",
-        "markdown_inline",
-        "printf",
-        "python",
-        "query",
-        "regex",
-        "toml",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "xml",
-        "yaml",
-      },
-
-      
-      -- Incremental Selection: Expand/shrink selection by syntax node
-      --   <C-space>: Start selection and expand to next node
-      --   <bs>: Shrink selection to previous node
-      --   scope_incremental: Disabled (set to false)
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",      -- Start selection
-          node_incremental = "<C-space>",   -- Expand to next node
-          scope_incremental = false,        -- Disable scope-based expansion
-          node_decremental = "<bs>",        -- Shrink to previous node
+        ensure_installed = {
+          "bash",
+          "c",
+          "diff",
+          "html",
+          "javascript",
+          "jsdoc",
+          "json",
+          "jsonc",
+          "lua",
+          "luadoc",
+          "luap",
+          "markdown",
+          "markdown_inline",
+          "printf",
+          "python",
+          "query",
+          "regex",
+          "toml",
+          "tsx",
+          "typescript",
+          "vim",
+          "vimdoc",
+          "xml",
+          "yaml",
         },
-      },
-    })
-  end,
+
+        
+        -- Incremental Selection: Expand/shrink selection by syntax node
+        --   <C-space>: Start selection and expand to next node
+        --   <bs>: Shrink selection to previous node
+        --   scope_incremental: Disabled (set to false)
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",      -- Start selection
+            node_incremental = "<C-space>",   -- Expand to next node
+            scope_incremental = false,        -- Disable scope-based expansion
+            node_decremental = "<bs>",        -- Shrink to previous node
+          },
+        },
+      })
+    end,
+  },
+
+  -- Auto-close and rename HTML/JSX tags
+  {
+    "windwp/nvim-ts-autotag",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {}, -- Use all default settings
+  },
 }
