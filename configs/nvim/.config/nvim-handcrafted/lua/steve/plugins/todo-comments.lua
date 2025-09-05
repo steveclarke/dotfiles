bc27611e614f941c33f57f9e86ee44c3f279ef93
Steve Clarke <steve@sevenview.ca>
2025-09-05 18:45:36 -0230

    Add nvim-surround plugin and update todo-comments configuration
    - Introduced a new Lua configuration file for the nvim-surround plugin, enabling easy surrounding of text objects in Neovim.
    - Updated the todo-comments plugin configuration to improve key mappings and added a setup call for enhanced functionality.

--- configs/nvim/.config/nvim-handcrafted/lua/steve/plugins/todo-comments.lua
+++ configs/nvim/.config/nvim-handcrafted/lua/steve/plugins/todo-comments.lua
@@ -2,14 +2,15 @@ return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    local todo = require("todo-comments")
-    todo.setup()
+    local todo_comments = require("todo-comments")
 
     vim.keymap.set("n", "]t", function()
-      todo.jump_next()
+      todo_comments.jump_next()
     end, { desc = "Jump to next todo comment" })
     vim.keymap.set("n", "[t", function()
-      todo.jump_prev()
+      todo_comments.jump_prev()
     end, { desc = "Jump to previous todo comment" })
+
+    todo_comments.setup()
   end
 }
