0acc24904890219369f1a7c56c12bb250fc27c32
Steve Clarke <steve@sevenview.ca>
2026-01-18 18:14:22 -0330

    feat: Add delta as git pager
    Configure delta for improved diff viewing with syntax highlighting
    and navigation support.

--- setups/git.sh
+++ setups/git.sh
@@ -3,3 +3,9 @@ git config --global user.name "${DOTFILES_GIT_USER_NAME}"
 git config --global user.email "${DOTFILES_GIT_USER_EMAIL}"
 git config --global pull.rebase false
 git config --global init.defaultBranch master
+
+# Delta pager settings
+git config --global core.pager delta
+git config --global interactive.diffFilter 'delta --color-only'
+git config --global delta.navigate true
+git config --global merge.conflictStyle zdiff3
