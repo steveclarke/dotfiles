20279e83cd1268a12148224d7c6b22daa2ccb56f
Steve Clarke <steve@sevenview.ca>
2026-04-15 19:51:41 -0230

    feat: add remaining Arch CLI install scripts
    Add install scripts for packages from custom Homebrew taps and
    missing standard tools. Outport and SuperThread scripts are staged
    for upcoming AUR packages (see outport#106, superthread#80).
    
    Closes #9

---
+++ install/arch/cli/heroku.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+installing_banner "heroku"
+omarchy-pkg-aur-add heroku-cli
---
+++ install/arch/cli/process-compose.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+installing_banner "process-compose"
+omarchy-pkg-aur-add process-compose-git
